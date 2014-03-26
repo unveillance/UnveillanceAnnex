11da318faef90a381859f833d24f04e87555150d
harlo <harlo.holmes@gmail.com>
2014-03-26 15:36:18 -0400

    splitting off informa dependencies

--- setup.sh
+++ setup.sh
@@ -7,24 +7,9 @@ ANNEX_DIR=/home/unveillance_remote
 mkdir $OLD_DIR/.monitor
 mkdir $ANNEX_DIR
 mkdir /var/run/sshd
-chmod +x run.sh
 
 echo annex_dir: $ANNEX_DIR >> $USER_CONFIG
 echo base_dir: $OLD_DIR >> $USER_CONFIG
-
-echo "**************************************************"
-echo "Installing FFMPEG"
-mv lib/FFmpeg /home/FFmpeg
-cd /home/FFmpeg
-./configure
-make
-make install
-
-cd $OLD_DIR
-
-echo "**************************************************"
-echo "Installing FFMPEG2THEORA"
-apt-get install -y ffmpeg2theora
 
 echo "**************************************************"
 echo "Installing ELASTICSEARCH"
@@ -40,15 +25,6 @@ do
 		break
 	fi
 done
-
-# build jpeg redaction
-echo "**************************************************"
-echo "Building JPEG Redaction library"
-cd lib/jpeg-redaction-library/lib
-make
-g++ -L $OLD_DIR/lib/jpeg-redaction-library/lib -lredact jpeg.cpp jpeg_decoder.cpp jpeg_marker.cpp debug_flag.cpp byte_swapping.cpp iptc.cpp tiff_ifd.cpp tiff_tag.cpp j3mparser.cpp -o $OLD_DIR/lib/jpeg-redaction-library/jpeg_r.out
-
-cd $OLD_DIR
 
 wget -O lib/anaconda.sh http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.9.1-Linux-x86_64.sh
 chmod +x lib/anaconda.sh
