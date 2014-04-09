b5129dafa7fdec6466d22eefd7932b173eaaf825
harlo <harlo.holmes@gmail.com>
2014-04-09 19:03:02 -0400

    more text stubs

--- lib/Worker/Tasks/Text/evaluate_text.py
+++ lib/Worker/Tasks/Text/evaluate_text.py
@@ -20,13 +20,34 @@ def evaluateText(task):
 	if not document.getFile(document.file_name): return
 	
 	content = document.loadAsset(document.file_name)
+	new_mime_type = None
 	
 	import json
 	try:
 		json_txt = json.loads(content)
-		document.mime_type = "application/json"
-		document.save()
+		new_mime_type = "application/json"
 		
 		print "THIS IS JSON"
 	except Exception as e:
 		print "NOT JSON: %s" % e
+	
+	task_path = None
+	if new_mime_type is not None:
+		document.mime_type = new_mime_type
+		document.save()
+		
+		from vars import MIME_TYPE_TASKS
+		if document.mime_type in MIME_TYPE_TASKS.keys():
+			task_path = MIME_TYPE_TASKS[document.mime_type][0]
+	else:
+		try:
+			task_path = MIME_TYPE_TASKS[document.mime_type][1]
+		except Exception as e: print e
+	
+	if task_path is not None:
+		from lib.Worker.Models.uv_task import UnveillanceTask
+		new_task = UnveillanceTask(inflate={
+			'doc_id' : document._id,
+			'task_path' : ,
+			'queue' : UUID})
+		new_task.run(task)
