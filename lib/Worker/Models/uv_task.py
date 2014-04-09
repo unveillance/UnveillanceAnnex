67b8fbac8c4a32d929187d92d75a5c90cc2fea4a
harlo <harlo.holmes@gmail.com>
2014-04-09 15:25:47 -0400

    celery

--- lib/Worker/Models/uv_task.py
+++ lib/Worker/Models/uv_task.py
@@ -1,3 +1,4 @@
+import os
 from importlib import import_module
 from multiprocessing import Process
 
@@ -6,7 +7,7 @@ from lib.Core.vars import EmitSentinel, UVDocType
 from Utils.funcs import printAsLog
 from lib.Worker.vars import TASKS_ROOT
 
-from conf import DEBUG
+from conf import DEBUG, BASE_DIR
 
 class UnveillanceTask(UnveillanceObject):
 	def __init__(self, inflate=None, _id=None):		
@@ -15,27 +16,27 @@ class UnveillanceTask(UnveillanceObject):
 			inflate['_id'] = generateMD5Hash()
 			inflate['uv_doc_type'] = UVDocType.TASK
 			inflate['status'] = 404
-			print inflate
 			
-		super(UnveillanceTask, self).__init__(_id=_id, inflate=inflate, emit_sentinels=[
-			EmitSentinel("ctx", "Worker", None)])
+		super(UnveillanceTask, self).__init__(_id=_id, inflate=inflate, 
+			emit_sentinels=[EmitSentinel("ctx", "Worker", None)])
 	
-	def run(self, ctx):		
+	def run(self, ctx):
 		# i.e. "lib.Worker.Tasks.Documents.evaluate_document"
 		task_path = ".".join([TASKS_ROOT, self.task_path])
 		p, f = task_path.rsplit(".", 1)
-		module = import_module(p)
+
 		try:
+			module = import_module(p)
 			func = getattr(module, f)
 			args = [(self,), ({'queue' :self.queue})]
-			# or: args = [(self,), (queue : self.queue)] ?
 			if DEBUG: print args
-					
+			
+			#from lib.Worker.Tasks.Documents.evaluate_document import evaluateDocument
+			#evaluateDocument.apply_async((self,), {'queue' : self.queue})
 			p = Process(target=func.apply_async, args=args)
 			p.start()
 		except Exception as e:
 			printAsLog(e)
-			return		
 	
 	def finish(self):
 		if DEBUG: print "task finished!"
