2a68885bd2ab43ca917d1e75e42f61732e283970
harlo <harlo.holmes@gmail.com>
2014-04-17 15:07:17 -0400

    funcs

--- lib/Worker/Utils/funcs.py
+++ lib/Worker/Utils/funcs.py
@@ -1,12 +1,33 @@
-import magic
+import magic, gzip
+from cStringIO import StringIO
 
-def getFileType(path_to_file):
+def getFileType(file, as_buffer=False):
 	m = magic.Magic(flags=magic.MAGIC_MIME_TYPE)
 	try:
-		mime_type = m.id_filename(path_to_file)
+		if not as_buffer:
+			mime_type = m.id_filename(file)
+		else:
+			mime_type = m.id_buffer(file)
+
 		m.close()
 		return mime_type
 		
 	except: pass
 	m.close()
 	return None
+
+def unGzipBinary(bin):
+	try:
+		with gzip.GzipFile(fileobj=StringIO(bin)) as G:
+			return G.read()
+			
+	except Exception as e:
+		return None
+
+def unGzipFile(path):
+	try:
+		with gzip.GzipFile(path, 'rb') as G:
+			return G.read()
+			
+	except Exception as e:
+		return None
