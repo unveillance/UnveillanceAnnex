bf0416d27367947cca9026b0255709617a6a492b
harlo <harlo@guardianproject.info>
2014-06-02 18:39:29 +0000

    batch update

--- lib/Worker/Models/uv_batch.py
+++ lib/Worker/Models/uv_batch.py
@@ -1,9 +1,10 @@
 from Models.uv_object import UnveillanceObject
 
 class UnveillanceBatch(UnveillanceObject):
-	def __init__(self, _id=None, inflate=None):
+	def __init__(self, _id=None, inflate=None, emit_sentinels=None):
 		if inflate is not None:
 			from lib.Core.Utils.funcs import generateMD5Hash
 			inflate['_id'] = generateMD5Hash(content="".join(inflate['documents']))
 		
-		super(UnveillanceBatch, self).__init__(_id=_id, inflate=inflate)
+		super(UnveillanceBatch, self).__init__(_id=_id, 
+			inflate=inflate, emit_sentinels=emit_sentinels)
