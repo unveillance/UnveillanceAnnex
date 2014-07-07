a4370a41be90baffe545463d54c717bf9d27385b
harlo <harlo.holmes@nytimes.com>
2014-07-07 15:54:50 +0000

    setup cleanup

--- setup.sh
+++ setup.sh
@@ -5,21 +5,19 @@ if [ $# -eq 0 ]
 then
 	LAUNCH_ANNEX=true
 	WITH_CONFIG=0
-	
-	pip install --upgrade fabric
 else
 	LAUNCH_ANNEX=false
 	WITH_CONFIG=$1
 fi
 
-#wget -O lib/anaconda.sh http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.0.1-Linux-x86_64.sh
-#chmod +x lib/anaconda.sh
+wget -O lib/anaconda.sh http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.0.1-Linux-x86_64.sh
+chmod +x lib/anaconda.sh
 echo "**************************************************"
 echo "Installing Python Framework via ANACONDA"
 
 #sleep 10
 
-#./lib/anaconda.sh
+./lib/anaconda.sh
 
 sleep 3
 source ~/.bashrc
