2abd621b57f513bd3a2726af76b8d1cec2911ff6
harlo <harlo.holmes@gmail.com>
2014-08-19 11:41:39 +0000

    minus logging

--- lib/Worker/Tasks/Text/preprocess_nlp.py
+++ lib/Worker/Tasks/Text/preprocess_nlp.py
@@ -26,7 +26,6 @@ def preprocessNLP(task):
 	#	1. get all the words (bag of words)
 	try:
 		texts = loads(document.loadAsset("doc_texts.json"))
-		if DEBUG: print "TEXTS:\n%s" % texts
 	except Exception as e:
 		print "ERROR GETTING DOC-TEXTS: %s" % e
 		print "\n\n************** %s [ERROR] ******************\n" % task_tag
