b3291e4d0d03ab85e0a8ae9744c162c52c52d7a2
harlo <harlo@guardianproject.info>
2014-09-19 14:36:58 +0000

    minor reorg of helper funcs

--- run_task.py
+++ run_task.py
@@ -1,22 +1,27 @@
-import os, requests
 from sys import argv, exit
 
-if __name__ == "__main__":
+def run_task(task_id):
 	from Utils.funcs import printAsLog
 	from lib.Worker.Models.uv_task import UnveillanceTask
 	
 	try:
-		task = UnveillanceTask(_id=argv[1])
+		task = UnveillanceTask(_id=task_id)
 	except Exception as e:
 		printAsLog("no task id.  quitting", as_error=True)
-		exit(1)	
+		return False	
 
+	import requests
 	from conf import HOST, API_PORT
+	
 	try:
 		r = requests.post("http://%s:%d/task/" % (HOST, API_PORT), 
 			data={ '_id' : task._id })
 	except Exception as e:
 		printAsLog(e, as_error=True)
-		exit(1)
-	
+		return False
+
+	return True
+
+if __name__ == "__main__":
+	if not run_task(argv[1]): exit(-1)
 	exit(0)
