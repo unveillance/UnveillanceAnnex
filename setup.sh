deadaf181c86402bf0d2727748931e748b4f27d4
harlo <harlo.holmes@gmail.com>
2014-10-10 15:46:05 -0400

    tightening up setup

--- setup.sh
+++ setup.sh
@@ -10,32 +10,32 @@ else
 	WITH_CONFIG=$1
 fi
 
-wget -O lib/anaconda.sh http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.0.1-Linux-x86_64.sh
-chmod +x lib/anaconda.sh
-echo "**************************************************"
-echo "Installing Python Framework via ANACONDA"
+sudo apt-get install -y gcc
 
-sleep 10
-./lib/anaconda.sh
-sleep 3
+PYTHON_VERSION=$(which python)
+if [[ $PYTHON_VERSION == *anaconda/bin/python ]]
+then
+	echo "ANACONDA already installed.  Skipping"
+else
+	wget -O lib/anaconda.sh http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.0.1-Linux-x86_64.sh
+	chmod +x lib/anaconda.sh
+	echo "**************************************************"
+	echo "Installing Python Framework via ANACONDA"
 
-ANACONDA=$(grep -i "anaconda" ~/.bashrc)
-echo $ANACONDA >> ~/.bash_profile
-source ~/.bash_profile
-sleep 3
+	sleep 10
+	./lib/anaconda.sh
+	sleep 3
+
+	ANACONDA=$(grep -i "anaconda" ~/.bashrc)
+	echo $ANACONDA >> ~/.bash_profile
+	source ~/.bash_profile
+	sleep 3
+fi
 
 pip install --upgrade -r requirements.txt
 
 cd lib/Core
 pip install --upgrade -r requirements.txt
-
-cd $THIS_DIR/lib
-mkdir dstk
-wget -O dstk/dstk.zip http://www.datasciencetoolkit.org/python_tools.zip
-unzip dstk/dstk.zip -d dstk
-rm -rf dstk/__MACOSX
-cd dstk/python
-python setup.py install
 
 cd $THIS_DIR
 echo "**************************************************"
