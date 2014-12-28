3c7d5ef802b05f2a9413697955d9e029c2250f40
harlo <harlo.holmes@gmail.com>
2014-12-28 08:02:25 -0500

    pulling query builder out of api space

--- Utils/funcs.py
+++ Utils/funcs.py
@@ -1,6 +1,185 @@
 import pandas, re
+from copy import deepcopy
 from datetime import datetime
+from time import mktime
 from fabric.api import settings, local
+
+from vars import QUERY_DEFAULTS, QUERY_KEYS
+from conf import DEBUG
+
+def build_els_query(args, query=None):
+	count_only = False
+	limit = None
+	cast_as = None
+	sort = None
+	doc_type = "uv_document"
+	exclude_fields = True
+
+	try:
+		doc_type = args['doc_type']
+		del args['doc_type']
+	except KeyError as e: pass
+
+	try:
+		exclude_fields = not args['get_all']
+		print exclude_fields
+		del args['get_all']
+	except KeyError as e: pass
+
+	try:
+		sort = args['sort']
+		del args['sort']
+	except KeyError as e: pass
+
+	if query is None:
+		try:
+			query = deepcopy(QUERY_DEFAULTS[doc_type.upper()])
+		except Exception as e:
+			if DEBUG:
+				print "could not find default query for %s" % doc_type.upper()
+
+			query = deepcopy(QUERY_DEFAULTS['MATCH_ALL'])
+	
+	if len(args.keys()) > 0:
+		# pull out variables that don't go in search
+		try:
+			count_only = args['count']
+			del args['count']
+		except KeyError as e: pass
+		
+		try:
+			limit = args['limit']
+			del args['limit']
+		except KeyError as e: pass
+		
+		try:
+			cast_as = args['cast_as']
+			del args['cast_as']
+		except KeyError as e: pass
+
+	if len(args.keys()) > 0:
+		# extend out top-level query
+		musts = []
+
+		for a in args.keys():
+			must = None
+			if a in QUERY_KEYS['match']:
+				must = {
+					"match" : { "%s.%s" % (doc_type, a) : args[a] }
+				}
+
+			elif a in QUERY_KEYS['filter_terms']:
+				must = {
+					"constant_score" : {
+						"filter" : {
+							"terms" : {
+								"%s.%s" % (doc_type, a) : args[a] if type(args[a]) is list else [args[a]]
+							}
+						}
+					}
+				}
+
+			elif a in QUERY_KEYS['filter_ids']:
+				must = {
+					"ids" : {
+						"type" : doc_type,
+						"values" : args[a] if type(args[a]) is list else [args[a]]
+					}
+				}
+
+				musts = [must]
+				del args[a]					
+				break
+
+			if must is not None:
+				del args[a]
+				musts.append(must)
+
+		if len(musts) > 0:
+			if 'match_all' in query.keys():
+				del query['match_all']
+
+			if musts[0].keys()[0] == "ids":
+				query = musts[0]
+			else:
+				if 'bool' not in query.keys():
+					query['bool'] = { "must" : [] }
+
+				if 'must' not in query['bool'].keys():
+					query['bool']['must'] = []
+
+				for must in musts: query['bool']['must'].append(must)
+
+	if len(args.keys()) > 0:
+		# this becomes a filtered query
+		query = {
+			"filtered" : {
+				"query" : query,
+				"filter" : {}
+			}
+		}
+		
+		filters = []
+		for a in args.keys():
+			filter = None
+
+			if 'term' in QUERY_KEYS and a in QUERY_KEYS['term']:
+				filter = { "term": { "%s.%s" % (doc_type, a) : args[a] }}
+
+			elif 'range' in QUERY_KEYS and a in QUERY_KEYS['range']:
+				try:
+					day = datetime.date.fromtimestamp(args[a]/1000)
+				except Exception as e:
+					print "TIME ERROR: %s" % e
+					continue
+					
+				if "upper" in args.keys():
+					lte = datetime.date.fromtimestamp(args['upper']/1000)
+					gte = datetime.date.fromtimestamp(args[a]/1000)
+				else:
+					lte = datetime.date(day.year, day.month, day.day + 1)
+					gte = datetime.date(day.year, day.month, day.day)
+				
+				filter = {
+					"range" : {
+						"%s.%s" % (doc_type, a) : {
+							"gte" : format(mktime(gte.timetuple()) * 1000, '0.0f'),
+							"lte" : format(mktime(lte.timetuple()) * 1000, '0.0f')
+						}
+					}
+				}
+
+			elif 'geo_distance' in QUERY_KEYS and a in QUERY_KEYS['geo_distance']:
+				if "radius" not in args.keys():
+					radius = 3
+				else:
+					radius = args['radius']
+
+				filter = {
+					"geo_distance" : {
+						"distance" : "%dmi" % radius,
+						"%s.%s" % (doc_type, a) : args[a]
+					}
+				}
+
+			if filter is not None:
+				filters.append(filter)
+
+		if len(filters) > 1:
+			query['filtered']['filter']['and'] = filters
+		else:
+			if len(filters) == 1:
+				try:
+					query['filtered']['filter'] = filters[0]
+				except Exception as e:
+					print "COULD NOT BUILD QUERY: %s" % e
+					return None
+
+	return (query, { 'doc_type' : doc_type if doc_type != "uv_document" else None,
+		'sort' : sort, 
+		'count_only' : count_only, 
+		'cast_as' : cast_as, 
+		'exclude_fields' : exclude_fields })
 
 def forceQuitUnveillance(target=None):
 	if target is None:
