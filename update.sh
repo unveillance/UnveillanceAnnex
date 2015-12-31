1cac68f2b7243bda3d8dbc2069f345c4e7743401
harlo <harlo.holmes@gmail.com>
2015-12-31 12:13:33 -0500

    updater script

---
+++ update.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+THIS_DIR=`pwd`
+PROJECT_DIR=../../
+
+# update config if changed
+update_config(){
+	echo "CONFIG UPDATE TBD"
+}
+
+# update setup scripts if changed
+update_setup(){
+	if [ $(ls $PROJECT_DIR/setup_extras)? -eq 0 ]; then
+		cd $PROJECT_DIR/setup && ./setup_extras.sh
+	fi
+}
+
+# update tasks if changed
+update_tasks(){
+	if [ $(ls $PROJECT_DIR/Tasks)? -eq 0 ]; then
+		cd $THIS_DIR/lib/Worker/Tasks
+		ln -s $PROJECT_DIR/Tasks/* .
+		ls -la
+	fi
+}
+
+# update models if changed
+update_models(){
+	if [ $(ls $PROJECT_DIR/Models)? -eq 0 ]; then
+		cd $THIS_DIR/lib/Worker/Models
+		ln -s $PROJECT_DIR/Models/* .
+		ls -la
+	fi
+}
+
+show_usage(){
+	echo "_________________________"
+	echo "Updater Help"
+	echo "_________________________"
+
+	echo "./update.sh [config|setup|tasks|models|all]"
+}
+
+case "$1" in
+	config)
+		update_config
+		;;
+	setup)
+		update_setup
+		;;
+	tasks)
+		update_tasks
+		;;
+	models)
+		update_models
+		;;
+	all)
+		update_config
+		update_setup
+		update_tasks
+		update_models
+		;;
+	*)
+		show_usage
+		exit 1
+esac
+
+cd $THIS_DIR && ./restart.sh
+exit 0
